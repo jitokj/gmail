45d61059e1e3812be82e84d752161c0ba948edb7
jito <jitokj3@gmail.com>
2021-01-01 15:29:46 +0530

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Header.css";
+
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt="gmail"
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
