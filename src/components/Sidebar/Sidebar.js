0dc1a44ba2238fb087d1734e587334b3f3071107
jito <jitokj3@gmail.com>
2021-01-02 23:01:56 +0530

    styled sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import { Button } from "@material-ui/core";
+import { Button, IconButton } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 
@@ -11,6 +11,9 @@ import LabelImportantIcon from "@material-ui/icons/LabelImportant";
 import NearMeIcon from "@material-ui/icons/NearMe";
 import NoteIcon from "@material-ui/icons/Note";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import PersonIcon from "@material-ui/icons/Person";
+import PhoneIcon from "@material-ui/icons/Phone";
+import DuoIcon from "@material-ui/icons/Duo";
 
 const Sidebar = () => {
   return (
@@ -33,6 +36,19 @@ const Sidebar = () => {
       <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
       <SidebarOption Icon={NoteIcon} title="Draft" number={54} />
       <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
