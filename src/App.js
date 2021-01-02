f3ed2473bf61c86044b5ab1c0b4c399a1b81f8b4
jito <jitokj3@gmail.com>
2021-01-02 14:42:10 +0530

    completed header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Sidebar />
     </div>
   );
 }
