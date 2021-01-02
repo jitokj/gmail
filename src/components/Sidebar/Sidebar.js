d9dcaa6ee796856bcbf7770cd90127635738d6e6
jito <jitokj3@gmail.com>
2021-01-02 14:57:28 +0530

    sidebar and compose

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,20 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 
+import AddIcon from "@material-ui/icons/Add";
+
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <Button
+        className="sidebar__compose"
+        startIcon={<AddIcon fontSize="large" />}
+      >
+        COMPOSE
+      </Button>
+    </div>
+  );
 };
 
 export default Sidebar;
