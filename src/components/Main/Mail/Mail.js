7ead73ddea0354d9452288b17fa5cb9ab80e4a48
jito <jitokj3@gmail.com>
2021-01-04 12:04:26 +0530

    added icons to mail

--- src/components/Main/Mail/Mail.js
+++ src/components/Main/Mail/Mail.js
@@ -1,8 +1,72 @@
+import { IconButton } from "@material-ui/core";
 import React from "react";
+import { useHistory } from "react-router-dom";
 import "./Mail.css";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 const Mail = () => {
-  return <div className="mail">MAil</div>;
+  const history = useHistory();
+  return (
+    <div className="mail">
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail__toolsRight">
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mail;
