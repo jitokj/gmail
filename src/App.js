3642b829a589fadde9188f7076b3619ec22c444f
jito <jitokj3@gmail.com>
2021-01-04 18:42:16 +0530

    compose button- redux

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Mail from "./components/Main/Mail/Mail";
 import EmailList from "./components/Main/EmailList/EmailList";
 import SendMail from "./components/SendMail/SendMail";
+import { useSelector } from "react-redux";
+import { selectSendMessageIsOpen } from "./features/mailSlice";
 
 function App() {
+  const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   return (
     <Router>
       <div className="app">
@@ -23,7 +26,7 @@ function App() {
             </Route>
           </Switch>
         </div>
-        <SendMail />
+        {sendMessageIsOpen && <SendMail />}
       </div>
     </Router>
   );
