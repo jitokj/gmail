7cf2ae2637600992edf30c68d3d544a00a493b8f
jito <jitokj3@gmail.com>
2021-01-05 11:33:36 +0530

    logout functionality

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,23 @@ import SearchIcon from "@material-ui/icons/Search";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import { useSelector } from "react-redux";
-import { selectUser } from "../../features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, selectUser } from "../../features/userSlice";
+import { auth } from "../../firebase";
 
 const Header = () => {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const signOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(logout());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className="header">
       <div className="header__left">
@@ -35,7 +47,7 @@ const Header = () => {
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar src={user?.photoUrl} />
+        <Avatar onClick={signOut} src={user?.photoUrl} />
       </div>
     </div>
   );
